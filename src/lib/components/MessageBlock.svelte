5fd29430f2cfb4d597573681c1b743261015786d
alipw <alifwide.workmail@gmail.com>
2023-01-21 14:54:44 +0700

    feat: add timestamp - better styling

--- src/lib/components/MessageBlock.svelte
+++ src/lib/components/MessageBlock.svelte
@@ -7,5 +7,5 @@
 <div class="bg-gray-100 p-4 rounded-md my-2">
   <div class="text-lg font-medium">{message.sender}</div>
   <div class="text-base">{message.message}</div>
-  <div class="text-xs text-gray-600">{message.timestamp}</div>
+  <div class="text-xs text-gray-600">{message.timestamp ? message.timestamp : "no date"}</div>
 </div>
